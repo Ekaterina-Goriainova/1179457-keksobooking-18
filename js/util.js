279634251add327b5853b689d15f86b27575141f
Ekaterina-Goriainova <Ekaterina-Goriainova@users.noreply.github.com>
2019-10-28 16:46:20 +0300

    keksobooking-0.13
    Задание: "Максимум подвижности"

--- js/util.js
+++ js/util.js
@@ -1,9 +1,16 @@
 'use strict';
 
 (function () {
+  var KEY_ENTER = 13;
+  var KEY_ESC = 27;
+
+  var getRandomNumber = function (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
   window.util = {
-    getRandomNumber: function (min, max) {
-      return Math.floor(Math.random() * (max - min)) + min;
-    }
+    getRandomNumber: getRandomNumber,
+    KEY_ENTER: KEY_ENTER,
+    KEY_ESC: KEY_ESC
   };
 })();
